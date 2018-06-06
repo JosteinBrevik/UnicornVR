33715376649e6f4b1a3840b4e22649f5febbe169
Jostein <jostein.brevik@gmail.com>
2018-06-06 13:07:36 +0800

    Add darkness score detection

--- Assets/Scripts/CameraLightDetection.cs
+++ Assets/Scripts/CameraLightDetection.cs
@@ -6,39 +6,37 @@ public class CameraLightDetection : MonoBehaviour
 {
 	WebCamTexture webCamTexture;
 	public GameObject renderQuad;
+	public int darknessResoution = 1000;
+	public TextMesh scoreText;
+	Renderer quadRenderer;
+	Texture2D photo;
 	void Start() {
-		Debug.Log ("Started");
 		webCamTexture = new WebCamTexture();
 		//renderer.material.mainTexture = webCamTexture;
 		webCamTexture.Play();
-		TakePhoto ();
-		Debug.Log ("Took pic");
+		quadRenderer = renderQuad.GetComponent<Renderer>() as Renderer;
+		photo = new Texture2D(webCamTexture.width, webCamTexture.height);
+		InvokeRepeating ("TakePhoto", 1f, 0.5f);
 	}
 
 	void Update(){
-		TakePhoto ();
+		
 	}
+
 	void TakePhoto(){
-
-		// NOTE - you almost certainly have to do this here:
-
-		//yield return new WaitForEndOfFrame(); 
-
-		// it's a rare case where the Unity doco is pretty clear,
-		// http://docs.unity3d.com/ScriptReference/WaitForEndOfFrame.html
-		// be sure to scroll down to the SECOND long example on that doco page 
-
-		Texture2D photo = new Texture2D(webCamTexture.width, webCamTexture.height);
 		photo.SetPixels(webCamTexture.GetPixels());
 		photo.Apply();
-		Renderer quadRenderer = renderQuad.GetComponent<Renderer>() as Renderer;
-		//renderQuad.transform.parent = this.transform;
-		//renderQuad.transform.localPosition = new Vector3(0.0f, 0.0f, 3.0f);
+		quadRenderer.material.SetTexture("_MainTex", photo); //Only to display image
+		CalculateDarkness(webCamTexture.GetPixels32());
+	}
 
-		quadRenderer.material.SetTexture("_MainTex", photo);
-		//Encode to a PNG
-		//byte[] bytes = photo.EncodeToPNG();
-		//Write out the PNG. Of course you have to substitute your_path for something sensible
-		//File.WriteAllBytes(your_path + "photo.png", bytes);
+	void CalculateDarkness(Color32[] pixels){
+		print (pixels.Length);
+		print (pixels);
+		int sum = 0;
+		for (int i = 0; i < pixels.Length; i+=darknessResoution) {
+			sum += pixels [i].r + pixels [i].b + pixels [i].g ; //Don't want alpha channel
+		}
+		scoreText.text = (sum * darknessResoution/pixels.Length).ToString();
 	}
 }
